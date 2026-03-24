c08ab955e863fce81df11418392b551963e985d0
Yuhei FUJITA <contact@fujita.dev>
2026-03-25 02:06:12 +0900

    Initial Commit

---
+++ clipper/clipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipperApp.swift
+//  clipper
+//
+//  Created by Yuhei FUJITA on 2026/03/25.
+//
+
+import SwiftUI
+
+@main
+struct clipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clipperTests/clipperTests.swift
@@ -0,0 +1,17 @@
+//
+//  clipperTests.swift
+//  clipperTests
+//
+//  Created by Yuhei FUJITA on 2026/03/25.
+//
+
+import Testing
+@testable import clipper
+
+struct clipperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
